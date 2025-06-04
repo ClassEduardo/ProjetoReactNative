aa3b9cc21008e34865528e06fb2ebd6edb0306cd
Eduardo <dudue0578@gmail.com>
2025-06-03 21:14:35 -0300

    Testando a tela de criação

--- App.js
+++ App.js
@@ -1,10 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useEffect } from 'react';
+import CriarServico from './src/screens/CriarServico';
+import { createTable } from './src/services/ServicoBD';
 
 export default function App() {
+  useEffect(() => {
+    createTable();
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <CriarServico />
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +24,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    paddingTop: 24,
   },
 });
