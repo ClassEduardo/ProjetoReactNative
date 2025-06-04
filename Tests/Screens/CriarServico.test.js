127d80cb9f7654a24dff2349727fb51bc7586471
Eduardo <dudue0578@gmail.com>
2025-06-03 22:03:33 -0300

    ajuste classes de teste

--- Tests/Screens/CriarServico.test.js
+++ Tests/Screens/CriarServico.test.js
@@ -1,5 +1,4 @@
 import renderer from 'react-test-renderer';
-import CriarServico from '../../src/Screens/CriarServico';
 import CriarServico from '../../src/screens/CriarServico';
 
 it('Renderiza corretamente', () => {
---
+++ Tests/Screens/RegistrarServico.test.js
@@ -0,0 +1,7 @@
+import renderer from 'react-test-renderer';
+import RegistrarServico from '../../src/screens/RegistrarServico';
+
+it('Renderiza corretamente', () => {
+    const tree = renderer.create(<RegistrarServico />).toJSON();
+    expect(tree).toBeTruthy();
+});
